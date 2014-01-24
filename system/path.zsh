9525eb13817bd2436dba841bca0b66c4f0510111
Jonah Williams <jonah@carbonfive.com>
2014-01-23 18:51:00 -0800

    Update PATH config locations

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,10 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
+# Node modules path and module executables
+export NODE_PATH="/usr/local/lib/node_modules"
+PATH=$PATH:/usr/local/share/npm/bin
